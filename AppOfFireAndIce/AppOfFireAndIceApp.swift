593809470db9db3e01eef2c1e08b51e0e2e2bb2c
Herman Willem Keuris <Herman.Keuris@standardbank.co.za>
2023-08-09 13:38:25 +0200

    Initial Commit

---
+++ AppOfFireAndIce/AppOfFireAndIceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOfFireAndIceApp.swift
+//  AppOfFireAndIce
+//
+//  Created by Herman Willem Keuris on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct AppOfFireAndIceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
